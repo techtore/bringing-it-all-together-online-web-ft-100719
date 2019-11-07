2b603b4c7a38ee2d9410a280ddf72e282eb2a82c
Victoria Hale <vic.hale74@gmail.com>
2019-11-07 20:24:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,4 +62,15 @@ class Dog
       self.new_from_db(row)
     end.first
   end
+  
+  def self.find_or_create_by
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else 
+      dog = self.create(name: name, breed: breed)
+    end
+      dog
+  end
 end
