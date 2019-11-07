1095018bc8f1b8673e485fe6a5b95ed166476c3f
Victoria Hale <vic.hale74@gmail.com>
2019-11-07 20:12:31 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,51 @@
+require 'pry'
+class Dog 
+  attr_accessor :name, :breed, :id
+  
+  
+  def initialize(attributes)
+    attributes.each do |key,value|
+      instance_variable_set("@#{key}",value) 
+      @id ||= nil
+    end
+  end
+  
+  def self.create_table 
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT);
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql= <<-SQL 
+    DROP TABLE IF EXISTS dogs;
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save 
+      sql= <<-SQL 
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id =  DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+  end
+  
+  def self.create(attributes)
+    dog = Dog.new(attributes) 
+    dog.save
+    dog 
+  end
+  
+  def self.new_from_db(attributes)
+    dog = self.new(attributes)
+    dog
+    
+  end
+end
